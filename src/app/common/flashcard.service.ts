f85c04d5ae97e3dfd51100cd183023e05ddc5538
brandorags <brandorags@users.noreply.github.com>
2020-03-30 22:24:22 -0500

    prototype for saving flashcard deck titles

--- src/app/common/flashcard.service.ts
+++ src/app/common/flashcard.service.ts
@@ -5,6 +5,17 @@ import { Injectable } from '@angular/core';
 })
 export class FlashcardService {
 
+  private readonly flashcardDeckTitleArrKey = 'flashcardDeckTitles';
+
   constructor() { }
+
+  getFlashcardDeckTitleArr(): string[] {
+    let titles = localStorage.getItem(this.flashcardDeckTitleArrKey);
+    return (titles) ? JSON.parse(titles) : [];
+  }
+
+  updateFlashcardDeckTitleArr(titles: string[]): void {
+    localStorage.setItem(this.flashcardDeckTitleArrKey, JSON.stringify(titles));
+  }
 
 }
