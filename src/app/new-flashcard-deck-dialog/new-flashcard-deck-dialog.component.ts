bf75fd89530471bb1e004cd77c16d70948724b20
brandorags <brandorags@users.noreply.github.com>
2020-04-07 23:04:11 -0500

    successful save of flashcard deck title from dialog

--- src/app/new-flashcard-deck-dialog/new-flashcard-deck-dialog.component.ts
+++ src/app/new-flashcard-deck-dialog/new-flashcard-deck-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 
+import { FlashcardService } from '../common/flashcard.service';
+
 @Component({
   selector: 'app-new-flashcard-deck-dialog',
   templateUrl: './new-flashcard-deck-dialog.component.html',
@@ -10,14 +12,37 @@ import { MatDialogRef } from '@angular/material/dialog';
 export class NewFlashcardDeckDialogComponent {
 
   title: string;
+  filename: string;
+  fileContents: string[];
 
   constructor(
-    public dialogRef: MatDialogRef<NewFlashcardDeckDialogComponent>
+    private dialogRef: MatDialogRef<NewFlashcardDeckDialogComponent>,
+    private flashcardService: FlashcardService
   ) { }
 
   saveFlashcardDeck(): void {
-    console.log(this.title);
+    this.flashcardService.saveFlashcardDeckTitle(this.title);
     this.closeDialog();
+  }
+
+  onFileSelected(): void {
+    const inputNode: any = document.querySelector('#file');
+  
+    if (typeof (FileReader) !== 'undefined') {
+      const reader = new FileReader();
+  
+      reader.onload = (e: any) => {
+        const fileResult = e.target.result;
+        const fileResultArr = fileResult.split('\n');
+        for (let a of fileResultArr) {
+          this.fileContents.push(a);
+        }
+      };
+  
+      const file = inputNode.files[0];
+      this.filename = file.name;
+      reader.readAsText(file);
+    }
   }
 
   closeDialog(): void {
