dfb40bc6be11d971af51150434cdc4c93e2f0ce4
Brandon Ragsdale <brandon.s.ragsdale@gmail.com>
2020-04-22 23:14:50 -0500

    generate questions and random answer choices

--- src/app/flashcard/flashcard.component.ts
+++ src/app/flashcard/flashcard.component.ts
@@ -12,6 +12,13 @@ import { FlashcardDeck } from '../models/flashcard-deck';
 export class FlashcardComponent implements OnInit {
 
   flashcardDeck: FlashcardDeck;
+  
+  answerKeys: string[] = [];
+  questionCounter: number = 0;
+  currentQuestion: string;
+  choiceOne: string;
+  choiceTwo: string;
+  choiceThree: string;
 
   constructor(private flashcardService: FlashcardService) { }
 
@@ -23,6 +30,65 @@ export class FlashcardComponent implements OnInit {
 
     this.flashcardDeck = this.flashcardService.getFlashcardDeck(title);
     this.flashcardService.saveLastAccessedFlashcardDeckTitle(title);
+
+    this.answerKeys = Array.from(this.flashcardDeck.flashcards.keys());
+
+    this.generateQuestion();
+  }
+
+  generateQuestion(): void {
+    this.showFlashcard();
+    this.randomizeAnswerChoices();
+  }
+
+  private showFlashcard(): void {
+    const flashcards = this.flashcardDeck.flashcards;
+    if (this.questionCounter > flashcards.size) {
+      return;
+    }
+
+    const answer = this.answerKeys[this.questionCounter];
+    this.currentQuestion = flashcards.get(answer);
+  }
+
+  private randomizeAnswerChoices(): void {
+    const deckCount = this.flashcardDeck.flashcards.size;
+    const correctAnswerChoice = this.getRandomNumber(1, 3);
+
+    while (true) {
+      const randomNum = this.getRandomNumber(0, deckCount);
+      this.choiceOne = (correctAnswerChoice === 1) ? 
+        this.answerKeys[this.questionCounter] : 
+        this.answerKeys[randomNum];
+      if (this.choiceOne) {
+        break;
+      }
+    }
+    
+    while (true) {
+      const randomNum = this.getRandomNumber(0, deckCount);
+      this.choiceTwo = (correctAnswerChoice === 2) ? 
+        this.answerKeys[this.questionCounter] : 
+        this.answerKeys[randomNum];
+      if (this.choiceTwo && this.choiceTwo !== this.choiceOne) {
+        break;
+      }
+    }
+
+    while (true) {
+      const randomNum = this.getRandomNumber(0, deckCount);
+      this.choiceThree = (correctAnswerChoice === 3) ? 
+        this.answerKeys[this.questionCounter] : 
+        this.answerKeys[randomNum];
+      if (this.choiceThree && this.choiceThree !== this.choiceOne &&
+        this.choiceThree !== this.choiceTwo) {
+        break;
+      }
+    }
+  }
+
+  private getRandomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
 }
